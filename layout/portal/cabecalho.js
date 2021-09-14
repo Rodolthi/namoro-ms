088437f395f71164af64f4a6bf823ce886c7c71e
thiagouxd <thiago.uxbr@gmail.com>
2021-09-14 20:45:21 -0300

    Botão de deslogar

--- layout/portal/cabecalho.js
+++ layout/portal/cabecalho.js
@@ -2,20 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import Icone from "components/icone";
 import { useRouter } from "next/router";
+import { Button } from "@material-ui/core";
 
 const Cabecalho = () => {
-  const router = useRouter()
+  const router = useRouter();
+  const deslogar = () => {};
 
   return (
     <CabecalhoDoPortal>
       <Logo src="/logo.svg" />
 
-      <Icone nome="account_circle" />
+      <ContainerUsuario>
+        <Usuario>
+          <Icone nome="account_circle" />
+          Thiago Menezes
+        </Usuario>
+        <Button onClick={deslogar} type="button">
+          <Icone nome="logout" />
+        </Button>
+      </ContainerUsuario>
     </CabecalhoDoPortal>
   );
 };
 
-export default Cabecalho
+export default Cabecalho;
 
 const CabecalhoDoPortal = styled.header`
   display: flex;
@@ -24,8 +34,29 @@ const CabecalhoDoPortal = styled.header`
   height: 64px;
   padding: 16px;
   background-color: var(--preta);
+  .MuiButton-root {
+    margin-left: 16px;
+  }
 `;
 
 const Logo = styled.img`
   height: 100%;
-`
+`;
+
+const ContainerUsuario = styled.div`
+  display: flex;
+  justify-content: center;
+  color: var(--branca);
+`;
+
+const Usuario = styled.p`
+  color: var(--branca);
+  font-size: 16px;
+  display: flex;
+  margin: 0;
+  height: 100%;
+  align-items: center;
+  .material-icons-round {
+    margin-right: 8px;
+  }
+`;
