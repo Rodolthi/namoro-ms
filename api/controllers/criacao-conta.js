a842cf69806392b284c109697561edd1320ba246
RodolfoPedra <rodolfoaldrovandip@gmail.com>
2021-09-21 19:53:51 -0400

    correçao criaçao e login de usuario

--- api/controllers/criacao-conta.js
+++ api/controllers/criacao-conta.js
@@ -4,8 +4,8 @@ import autenticar from 'api/services/autenticar';
 export const postUsuario = async (dataUser) => {
 
     const { data } = await autenticar({
-        "username": "thiago.uxbr@gmail.com",
-        "password": "*b!nM)yoa4QQtSWIeziQaOH("
+        "username": "rodolfoaldrovandip@gmail.com",
+        "password": "admin"
     });
     const result = await postData(dataUser, data.data.token);
     return result;
